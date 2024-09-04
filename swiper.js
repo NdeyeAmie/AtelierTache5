c753fa4dd3619fb927ef7943a56b99bf3c2d33e9
NdeyeAmie <tndeyeamie456@gmail.com>
2024-09-04 15:47:24 +0000

    two-commit

--- swiper.js
+++ swiper.js
@@ -1,22 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 30,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable : true,
-      dynamicBullets : true
-    },
+// const swiper = new Swiper('.swiper', {
+//     loop: true,
+//     grabCursor: true,
+//     spaceBetween: 30,
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable : true,
+//       dynamicBullets : true
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-      slidesPerView: 3,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//       slidesPerView: 3,
     
-  });
+//   });
 
 
   
